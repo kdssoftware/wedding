2a367a02a80309895a6234a8ef68efcd1b3d8dc1
= <=>
2022-04-27 16:46:30 +0200

    :bug: fixed layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,18 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <>
-      <div className='text-white text-8xl text-center w-screen mt-10 absolute shadow-black [text-shadow:0_4px_8px_rgba(0,0,0,0.85)]' >
-        Tosia & Karel
-      </div>
-      <div className='grid place-items-center h-screen pb-72'>
-        <div className="text-6xl text-center text-white bg-amber-500 bg-opacity-60 rounded-lg p-6 ">
-            Currently developing this website.
+      <div className='grid col-span-3'>
+        <div className='text-white text-7xl text-center w-screen shadow-black py-5 [text-shadow:0_4px_8px_rgba(0,0,0,0.85)]' >
+          Tosia & Karel
         </div>
+        <div className='grid place-items-center h-screen pb-72'>
+          <div className="text-6xl text-center text-white bg-amber-500 bg-opacity-60 rounded-lg p-6 ">
+              Currently developing this website.
+          </div>
+        </div>
+        <div></div>
       </div>
+
     </>
   )
 }
