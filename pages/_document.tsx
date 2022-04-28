69c85e2cc6b8cedb8349694a1bd7980b58515bed
= <=>
2022-04-28 23:40:06 +0200

    :lipstick: Update styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="manifest" href="/app.webmanifest" crossOrigin="use-credentials"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"crossOrigin"}/>
-        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Cormorant+Garamond:ital,wght@1,300&family=Italianno&family=League+Script&family=Ms+Madi&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Italianno&family=League+Script&family=Ms+Madi&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,42 @@
 import type { NextPage } from 'next'
 
-
 const Home: NextPage = () => {
- 
-  const time = (value:string) => (<span className='font-font3 text-3xl font-bold'>{value}</span>)
+  
+  const time = (value:string,fontSize:number=4) => (<span className={'font-font3 not-italic text-'+fontSize+'xl font-bold'}>{value}</span>)
   
   return (
     <>
-      <div className='grid col-span-3 font-font2'>
-        <h1 className='text-white text-maxxl text-center w-screen shadow-black py-5 [text-shadow:0_4px_8px_rgba(0,0,0,0.85)]' >
+      <div className='font-font2 [text-shadow:0_4px_8px_rgba(0,0,0,0.65)] italic'>
+
+        <span className='hidden w-0 h-0 text-1xl text-2xl text-3xl text-4xl text-5xl text-6xl text-7xl text-8xl text-9xl [text-shadow:0_4px_8px_rgba(0,0,0,0.85)]'></span>
+
+        <h1 className='text-white text-maxxl text-center w-screen shadow-black py-5 ' >
           T&K
         </h1>
-        <div className='grid place-items-center h-screen pb-72'>
-          <div className="  w-4/5 h-4/5 text-white bg-[#ffd27f] bg-opacity-40 rounded-lg p-6 ">
+
+        <div className='grid place-items-center h-screen pb-4'>
+          <div className="w-full text-white bg-[#c19440] bg-opacity-60 rounded-lg p-6 ">
               <h2 className='text-6xl text-center'>Welkom!</h2>
               <hr />
-              <p className='text-3xl text-center'>
-                Wij gaan trouwen op 13 augustus
+              <p className='text-4xl text-center'>
+                Wij gaan trouwen op {time("13 augustus",5)}
               </p>
-              <br />
-              <div className='text-2xl'>
-                <p >
-                  We verwelkomen je graag om {time("12u30")} op de Oude Markt in Leuven.
-                </p>
-                <p>
-                  De receptie start om {time("13u00")} aan Museum M.
-                </p>
-              </div>
           </div>
         </div>
-        <div></div>
+        
+        <nav className="fixed w-screen h-12 bottom-0 bg-orange-300 bg-opacity-40 grid grid-cols-3 gap-2 justify-center text-center ">
+          {
+              [
+                  "info",
+                  "RSVP",
+                  "Tips"
+              ].map( (title, key) => (
+                  <div className="text-4xl text-white cursor-pointer hover:scale-125 transition-all duration-200 ease-in-out " key={key}>
+                      {title}
+                  </div>
+              ) )
+          }
+      </nav>
       </div>
 
     </>
